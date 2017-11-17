b02246f86ada8b76e7f83138b7f09bf32b063af1
Sohaib <arif_sohaib@outlook.com>
2017-11-17 16:30:48 -0500

    added prediction using inception autoencoder

--- keras/inception_autoencoder_predict.py
+++ keras/inception_autoencoder_predict.py
@@ -1,6 +1,7 @@
 from inception_autoencoder import build_autoencoder
 import numpy as np
-
+import pickle
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -15,7 +16,8 @@ def main():
 
     mse_full = ((predict_full - full_data) ** 2).mean(axis=1)
     mse_impact = ((predict_impact - impact_data) ** 2).mean(axis=1)
-    print("full mse avg {}".format(mse_full.mean()))
+    mean_mse = mse_full.mean()
+    print("full mse avg {}".format(mean_mse))
     print("impact mse avg {}".format(mse_impact.mean()))
 
     print("full mse min {}".format(mse_full.min()))
@@ -24,13 +26,48 @@ def main():
     print("full mse max {}".format(mse_full.max()))
     print("impact mse max {}".format(mse_impact.max()))
 
+    plt.hist(mse_full)
+    plt.show()
+    plt.hist(mse_impact)
+    plt.show()
+
+    full_percentile = np.percentile(mse_full, 75)
+    impact_percentile = np.percentile(mse_impact, 75);
+    print("full mse 25 {}".format(full_percentile))
+    print("impact mse 25 {}".format(impact_percentile))
+    
+    print("length of full data {}".format(len(mse_full)))
     pred_impact_idx = []
     #running the above statistics, we can say that if the mse is above the max of impact mse, then it is not an impact
     for idx, err in enumerate(mse_full):
-        if err > mse_impact:
+        # if err > mse_impact.max():
+        #     pred_impact_idx.append(idx)
+        if err > full_percentile:
             pred_impact_idx.append(idx)
 
+    
+    with open('../data/frames/frames_vid1_div5.pkl', 'rb') as f:
+        confirmed_idx = pickle.load(f)
+    confirmed_idx = sorted(confirmed_idx.values())
+    """
+    for each value in confirmed_idx we need 10 numbers before and 10 after it(totaling 29 * 20 = 580)
+    """
+    full_idx = []
+    for idx in confirmed_idx:
+        for i in range(-10, 10):
+            full_idx.append(idx+i)
+
+    true_count = 0
+    false_pos = 0
     #to check accuracy, we can compare against idx's computed before
+    for idx in pred_impact_idx:
+        if idx in full_idx:
+            true_count += 1
+        else:
+            false_pos += 1
+    print("true count {}".format(true_count))
+    print("length of pred impacts {}".format(len(full_idx)))
+    print("false pos {}".format(false_pos))
     
 if __name__ == '__main__':
     main()
