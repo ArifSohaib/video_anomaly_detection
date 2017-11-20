ebb4259087e3c6ddba4f2045cb11a7fef7446104
Sohaib <arif_sohaib@outlook.com>
2017-11-20 16:01:06 -0500

    working inception_autoencoder

--- keras/inception_autoencoder_predict.py
+++ keras/inception_autoencoder_predict.py
@@ -2,12 +2,16 @@ from inception_autoencoder import build_autoencoder
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 def main():
 
     impact_data = np.loadtxt('../data/features/impacts_period1.npy')
     full_data = np.loadtxt('../data/features/period1_full.npy')
-    
+    min_max_scaler = preprocessing.MinMaxScaler()
+    impact_data = min_max_scaler.fit_transform(impact_data)
+    full_data = min_max_scaler.fit_transform(full_data)
+
     model = build_autoencoder()
     model.load_weights('../data/weights/autoencoder_weights.h5')
 
@@ -26,23 +30,21 @@ def main():
     print("full mse max {}".format(mse_full.max()))
     print("impact mse max {}".format(mse_impact.max()))
 
-    plt.hist(mse_full)
+    plt.hist(mse_full, label='full_mse stats')
     plt.show()
-    plt.hist(mse_impact)
+    plt.hist(mse_impact, label='impact_mse stats')
     plt.show()
 
-    full_percentile = np.percentile(mse_full, 75)
-    impact_percentile = np.percentile(mse_impact, 75);
-    print("full mse 25 {}".format(full_percentile))
-    print("impact mse 25 {}".format(impact_percentile))
+    full_percentile = np.percentile(mse_full, 50)
+    impact_percentile = np.percentile(mse_impact, 50);
+    print("full mse percentile {}".format(full_percentile))
+    print("impact mse percentile {}".format(impact_percentile))
     
     print("length of full data {}".format(len(mse_full)))
     pred_impact_idx = []
     #running the above statistics, we can say that if the mse is above the max of impact mse, then it is not an impact
     for idx, err in enumerate(mse_full):
-        # if err > mse_impact.max():
-        #     pred_impact_idx.append(idx)
-        if err > full_percentile:
+        if err > impact_percentile:
             pred_impact_idx.append(idx)
 
     
@@ -60,11 +62,14 @@ def main():
     true_count = 0
     false_pos = 0
     #to check accuracy, we can compare against idx's computed before
+    idx_count = 0;
     for idx in pred_impact_idx:
         if idx in full_idx:
             true_count += 1
+            
         else:
             false_pos += 1
+    print("num predictions {}".format(len(pred_impact_idx)))
     print("true count {}".format(true_count))
     print("length of pred impacts {}".format(len(full_idx)))
     print("false pos {}".format(false_pos))
